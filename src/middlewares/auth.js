8bd113bebc0ede2ff8d0cb9c8547d72a7a9e3140
Giovani Boff <giovanisouza15@gmail.com>
2021-03-23 08:09:35 -0300

    fix header token split

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,14 +4,16 @@ import authConfig from '../config/auth';
 
 export default async (req, res, next) => {
 
-    const authHeader = req.header.authorization;
+    const authHeader = req.headers.authorization;
 
     if (!authHeader) {
         return res.status(401).json({ error: 'Token not provided' });
     }
 
-    const [, token] = authHeader.split('');
+    const [, token] = authHeader.split(' ');
+    console.log(token);
     try {
+
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
         req.userId = decoded.id;
