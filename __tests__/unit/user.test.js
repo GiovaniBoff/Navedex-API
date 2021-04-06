5b83076fa71aa9e43141b95c6eae30d8d651cfef
Giovani Boff <giovanisouza15@gmail.com>
2021-04-06 19:14:23 -0300

    fix user unit test

--- __tests__/unit/user.test.js
+++ __tests__/unit/user.test.js
@@ -1,21 +1,21 @@
 import bcrypt from 'bcryptjs';
-import sequelize from 'sequelize';
 import User from '../../src/models/User';
+import database from '../../src/database/index';
 
-describe("User unit", () => {
+/*eslint-disable */
+describe('User unit', () => {
     beforeEach(async () => {
-        await User.sync({ force: true });
-    })
+        await database.init();
+    });
 
-    it("should encrypt user password", async () => {
-
+    it('should encrypt user password', async () => {
         const user = await User.create({
-            name: "TestUser",
-            email: "mail@mail.com",
-            password: "123"
-        });
+        name: 'TestUser',
+        email: 'mail@mail.com',
+        password: '123',
+    });
 
-        const compareHash = await bcrypt.compare("123", user.password_hash);
+        const compareHash = await bcrypt.compare('123', user.password_hash);
 
         expect(compareHash).toBe(true);
     });
