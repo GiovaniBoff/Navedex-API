5d48baa860f013d9f32db5cb98cdbddfe36e82c6
Giovani Boff <giovanisouza15@gmail.com>
2021-04-06 21:28:57 -0300

    fix github action file

--- __tests__/integration/userAuthentication.test.js
+++ __tests__/integration/userAuthentication.test.js
@@ -3,59 +3,50 @@ import request from 'supertest';
 import app from '../../src/app';
 
 import User from '../../src/models/User';
-
+/*eslint-disable */
 describe('User authentication', () => {
-    it('should authenticate with valid credentials', async () => {
-        const user = await User.create({
-            name: "TestUser",
-            email: "test@mail.com",
-            password: "123"
-        });
-
-        const response = await request(app)
-            .post("/login")
-            .send({
-                email: user.email,
-                password: user.password
-            });
-
-        expect(response.status).toBe(200);
+  it('should authenticate with valid credentials', async () => {
+    const user = await User.create({
+      name: 'TestUser',
+      email: 'test@mail.com',
+      password: '123',
     });
 
-    it('should not authenticate with invalid credentials', async () => {
-        const user = await User.create({
-            name: "TestUser",
-            email: "test2@mail.com",
-            password: "1234"
-        });
-
-        const response = await request(app)
-            .post("/login")
-            .send({
-                email: user.email,
-                password: "321"
-            });
-
-        expect(response.status).toBe(400);
+    const response = await request(app).post('/login').send({
+      email: user.email,
+      password: user.password,
     });
 
-    it('should return jwt token when autheticated', async () => {
-        const user = await User.create({
-            name: "TestUser",
-            email: "test3@mail.com",
-            password: "1234"
-        });
+    expect(response.status).toBe(200);
+  });
 
-        const response = await request(app)
-            .post("/login")
-            .send({
-                email: user.email,
-                password: user.password
-            })
-
-        expect(response.body).toHaveProperty('token');
+  it('should not authenticate with invalid credentials', async () => {
+    const user = await User.create({
+      name: 'TestUser',
+      email: 'test2@mail.com',
+      password: '1234',
     });
 
+    const response = await request(app).post('/login').send({
+      email: user.email,
+      password: '321',
+    });
 
+    expect(response.status).toBe(400);
+  });
 
+  it('should return jwt token when autheticated', async () => {
+    const user = await User.create({
+      name: 'TestUser',
+      email: 'test3@mail.com',
+      password: '1234',
+    });
+
+    const response = await request(app).post('/login').send({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(response.body).toHaveProperty('token');
+  });
 });
